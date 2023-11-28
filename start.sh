bc4b640b30b80eb4b94890a404ad26359b4251e4
jkkiig <151501362+jkkiig@users.noreply.github.com>
2023-11-28 15:15:55 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ REG_TOKEN=$REG_TOKEN
 
 cd /home/docker/actions-runner
 
-./config.cmd --url https://github.com/jkkiig/github-actions-demo --token BED3UMUVL6IBPIS3PTAS3PDFMWVNM
+./config.sh --url https://github.com/jkkiig/github-actions-demo --token BED3UMWOPV2RS77PYDJJDGDFMWX2U
 
 ./run.sh & wait $!
